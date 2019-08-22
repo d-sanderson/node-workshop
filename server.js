9d47788efb920c416cb1be28a8ef9583fc5dca9d
dsanderson90 <dsanderson3@cnm.edu>
2019-08-22 13:59:35 -0600

    cleaned up ex1

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 // TODO: create a basic server with express
-// that will send back the index.html file on a GET request to '/'
-// it should then send back jsonData on a GET to /data
-
-
 const express = require('express');
 const app = express();
 
+
 let jsonData = {count: 12, message: 'hey'};
 
+// that will send back the index.html file on a GET request to '/'
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
+// it should then send back jsonData on a GET to /data
 app.get('/data', (req, res) => {
   res.json(jsonData);
 });
 
-
-app.listen(3000, () => console.log('SERVER UP!!'))
-
+app.listen(3000, () => console.log('SERVER UP!!'));
