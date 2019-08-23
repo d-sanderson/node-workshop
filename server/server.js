fcac8d93c6e8ba1fe2f0ba5718f88a6a4e2e13be
dsanderson90 <dsanderson3@cnm.edu>
2019-08-22 22:59:55 -0600

    completed exercise 2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 // TODO: make this work.
-// if yuo go to localhost:3000 the app
+// if you go to localhost:3000 the app
 // there is expected crud to be working here
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -13,15 +13,47 @@ var _ = require('lodash');
 app.use(express.static('client'));
 
 // body parser makes it possible to post JSON to the server
-// we can accss data we post on as req.body
+// we can access data we post on as req.body
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-var lions = [];
+var lions = [
+
+];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
 
+//  create
+app.post('/lions', (req, res) => {
+  let lion = req.body;
+  id++;
+  lion.id = id + '';
+  lions.push(lion)
+  res.json(lion)
+});
+
+//  read
+app.get('/lions', (req, res) => {
+res.json(lions);
+
+});
+
+app.get('/lions/:id', (req, res) => {
+let lion = _.find(lions, {id: req.params.id});
+res.json(lion || {});
+
+});
+// //  update
+app.put('/update/:id', (req, res) => {
+
+
+});
+// //  destroy
+app.delete('/delete', (req, res) => {
+
+
+});
 app.listen(3000);
 console.log('on port 3000');
