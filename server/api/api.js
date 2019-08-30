280b369d363f67ba34f0b838c8ef99dca68e7dbf
dsanderson90 <dsanderson3@cnm.edu>
2019-08-30 13:28:40 -0600

    completed exercise 6

--- server/api/api.js
+++ server/api/api.js
@@ -1,12 +1,15 @@
-var router = require('express').Router();
+const router = require('express').Router();
+const usersRouter = require('./user/userRoutes');
+const catesRouter = require('./category/categoryRoutes');
+const postsRouter = require('./post/postRoutes');
 
 // api router will mount other routers
 // for all our resources. Each resource directory
 // has a resourceRoutes.js file with the router ready to go,
 // require them and mount them to their respective routes below
 
-router.use('/users', /* require the router*/);
-router.use('/categories', /* require the router*/);
-router.use('/posts', /* require the router*/);
+router.use('/users', usersRouter);
+router.use('/categories', catesRouter);
+router.use('/posts', postsRouter);
 
 module.exports = router;
--- server/middleware/appMiddlware.js
+++ server/middleware/appMiddlware.js
@@ -1,5 +1,5 @@
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
 // setup global middleware here
 
 module.exports = function(app) {
