c8046a76a7d053c7687158c8b8afcadaba5ff647
dsanderson90 <dsanderson3@cnm.edu>
2019-08-30 12:54:27 -0600

    started exercise 6

--- server/server.js
+++ server/server.js
@@ -1,29 +1,13 @@
-// TODO: mount the tigers route with a a new router just for tigers
-// exactly like lions below
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-var _ = require('lodash');
-var morgan = require('morgan');
-app.use(morgan('dev'))
-app.use(express.static('client'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+var api = require('./api/api');
 
-var lionRouter = require('./lions');
-var tigerRouter = require('./tigers');
+// setup the app middlware
+require('./middleware/appMiddlware')(app);
 
-// this is called mounting. when ever a req comes in for
-// '/lion' we want to use this router
-app.use('/lions', lionRouter);
-app.use('/tigers', tigerRouter)
+// setup the api
+app.use('/api/', api);
+// set up global error handling
 
-app.use(function(err, req, res, next) {
-  if (err) {
-    res.status(500).send(err);
-  }
-});
-
-
-app.listen(3000);
-console.log('on port 3000');
+// export the app for testing
+module.exports = app;
